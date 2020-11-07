4f5222edcdb4a4b7c4c53178355f9a20f2168fde
EC2 Default User <ec2-user@ip-172-31-8-173.us-east-2.compute.internal>
2020-11-07 03:11:02 +0000

    Remove unused files

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express(),
 path = require('path'),
-publicDir = path.join(__dirname,'public');
 
-app.use(express.static(publicDir))
+app.get('/', function(req, res, next){
+    res.send('Hello, world!');
+})
 
 app.listen(port);
 module.exports = app;
